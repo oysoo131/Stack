9da9db3699e9b3ce9b1279dac7e0e47f8a12f51f
oysoo131 <104242001+oysoo131@users.noreply.github.com>
2025-03-03 19:03:11 +0900

    Update Stack.lua

--- Stack.lua
+++ Stack.lua
@@ -14,7 +14,7 @@ function Stack:Window(Config:{})
 	Config = Config or {}
 	local Name = Config.Name or "Stack"
 	
-	StackLib["GUI"]= e("ScreenGui",{Name=Name,Parent=game.Players.LocalPlayer.CoreGui})
+	StackLib["GUI"]= e("ScreenGui",{Name=Name,Parent=game.CoreGui})
 	StackLib["Main Frame"]= e("Frame",{Name="Main Frame",Parent=StackLib["GUI"],Size=UDim2.new(0, 500, 0, 300),Position = UDim2.new(0.5,0,0.5,0),AnchorPoint=Vector2.new(0.5,0.5),BackgroundColor3=Color3.new(0.156863, 0.156863, 0.156863)})
 	e("UICorner",{Parent=StackLib["Main Frame"],CornerRadius=UDim.new(0,16)})
 	e("Frame",{Position=UDim2.new(0.5, 0,0.14, 0),Parent= StackLib["Main Frame"],Size=UDim2.new(0, 500,0, 2),BackgroundColor3=Color3.fromRGB(170, 170, 170),BorderSizePixel=0,AnchorPoint=Vector2.new(0.5, 0.5),ZIndex=2,Name="Line"})
